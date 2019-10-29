35f22aa9b2623f69bbe279716b9828c2c8fbcb6a
omid <omidekz@gmail.com>
2019-10-29 09:43:04 +0330

    mv password generator class to this file

--- passwordgeneratorclass.py
+++ passwordgeneratorclass.py
@@ -1,6 +1,4 @@
 from random import randint as ri, sample, shuffle
-import argparse
-
 
 class PasswordGenerator:
     smalls = list(map(chr, range(ord('a'), ord('z') + 1)))
@@ -66,26 +64,3 @@ class PasswordGenerator:
     def __repr__(self):
         return self.get_password()
 
-
-def main():
-    parser = argparse.ArgumentParser(description='Password Generator')
-    parser.add_argument('--max', type=int,
-                        help='Max length of password (default={})'.format(PasswordGenerator.MAX_LENGTH), default=-1)
-    parser.add_argument('--min', type=int,
-                        help='Min length of password (default={})'.format(PasswordGenerator.MIN_LENGTH), default=-1)
-    parser.add_argument('--len', type=int, help='Fix length of password (default=random between [min, max])',
-                        default=-1)
-
-    args = parser.parse_args()
-
-    minl = PasswordGenerator.MIN_LENGTH if args.min == -1 else args.min
-    maxl = PasswordGenerator.MAX_LENGTH if args.max == -1 else args.max
-
-    if args.len != -1:
-        minl, maxl = args.len, args.len
-    password = PasswordGenerator(min_length=minl, max_length=maxl).generate()
-    print(len(password), password)
-
-
-if __name__ == '__main__':
-    main()
